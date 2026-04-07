d74427a40a534a60a4b335a7e9ff7da13312641d
Aneffati <968840@dpsk12.org>
2026-04-07 10:41:29 -0600

    Initalized the window for the project

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
+import javax.swing.*;
 
-class Main {
+// Main class
+public class Main {
+
+    // Main driver method
     public static void main(String[] args) {
         
+        // Creating instance of JFrame
+        JFrame frame = new JFrame("CSA Unit 8 Project");
+
+        // Make the JFrame windowed fullscreen
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+        
+        // using no layout managers
+        frame.setLayout(null);
+
+        // making the frame visible
+        frame.setVisible(true);
+        
+        // Exit on close
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
     }
 }
